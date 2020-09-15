c9f8be0f224f05dee143389e794348b5894fb373
Hyeonjeong Lee <hlee220@myseneca.ca>
2020-09-15 11:16:15 +0900

    04_Person add height,address

--- gitTest/src/com/git/model/vo/Person.java
+++ gitTest/src/com/git/model/vo/Person.java
@@ -5,6 +5,10 @@ public class Person {
 	//회원정보를 저장하는 클래스
 	private String name;
 	private int age;
+	private String address;
+	private double height;
+	
+	
 	public Person() {
 		super();
 		// TODO Auto-generated constructor stub
@@ -20,6 +24,18 @@ public class Person {
 	}
 	public void setAge(int age) {
 		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public void setHeight(double height) {
+		this.height = height;
 	}
 	
 	
